77c223551e8cd4db32fdb5752356e80df3fdfb78
SUNDAR <aaransoftware@gmail.com>
2025-06-21 09:15:21 +0530

    #11 fixing folder

--- setup-v1.bat
+++ setup-v1.bat
@@ -40,7 +40,7 @@ mkdir prefiq >nul 2>&1
 
 > prefiq\create_superuser.py (
     echo import os
-    echo os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    echo os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     echo import django
     echo django.setup()
     echo from django.contrib.auth import get_user_model
